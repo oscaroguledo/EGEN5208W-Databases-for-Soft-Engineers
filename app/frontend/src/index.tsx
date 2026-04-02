1c0cc4116e3270e1d95bb1aba5b8700055106228
Oscar Oguledo <OSCARCHIAGOZIEM@GMAIL.COM>
2026-04-02 13:28:30 -0400

    Add missing CSS import in index.tsx

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from '@/App';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from '@/components/ThemeProvider';
+import './index.css';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
